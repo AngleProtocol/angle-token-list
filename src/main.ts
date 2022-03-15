30c0b352a971b3f088a6131aac1068d9887d6583
greedythib <thibaud.bruyelle@gmail.com>
2022-03-15 16:33:46 +0100

    cleaning

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { ChainId } from "@angleprotocol/sdk";
-import { TokenInfo, TokenInfoListType, TokenList } from "./types";
-import { token_list as mainnet_token_list } from "./chains/mainnet";
-
-export const TOKEN_LIST: TokenList = {
-  [ChainId.MAINNET]: mainnet_token_list,
-};
