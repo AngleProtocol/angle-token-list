449958d38e1d1a9319978be7853f0d39facfad7e
David <teddav@users.noreply.github.com>
2022-06-02 12:52:27 +0200

    Add JSON schema validation (#15)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import { ChainId } from "@angleprotocol/sdk";
-import { TokenInfo, TokenInfoListType, TokenList } from "./types";
-import { token_list as mainnet_token_list } from "./chains/mainnet";
+import { TokenList, joiSchema } from "./types";
+import erc20s from "../ERC20_LIST.json";
+import { inspect } from "util";
 
-export const TOKEN_LIST: TokenList = {
-  [ChainId.MAINNET]: mainnet_token_list,
-};
+export const TOKEN_LIST: TokenList[] = erc20s;
+
+const result = joiSchema.validate(TOKEN_LIST);
+if (result.error) {
+  console.log(inspect(result.error, { showHidden: false, depth: null, colors: true }));
+  throw new Error("wrong JSON format");
+} else {
+  console.log("all good");
+}
