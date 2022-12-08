50738d2816b152a197757ad6b1f77626ef903690
Picodes <41673773+Picodes@users.noreply.github.com>
2022-12-08 15:45:05 +0100

    feat: Wrap Native Token (#40)

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import ERC20_LIST from "../ERC20_LIST.json";
 import { inspect } from "util";
 import { utils } from "ethers";
 
-export const TOKEN_LIST: TokenList = ERC20_LIST[0];
+export const TOKEN_LIST: TokenList = ERC20_LIST[0] as TokenList;
 
 const result = joiSchema.validate(TOKEN_LIST);
-for (const chainId of Object.keys(TOKEN_LIST)) {
-  for (const key of Object.keys(TOKEN_LIST[parseFloat(chainId)])) {
-    if (utils.getAddress(key) !== key) {
+for (const chainIdString of Object.keys(TOKEN_LIST)) {
+  const chainId = parseFloat(chainIdString)
+  for (const key of Object.keys(TOKEN_LIST[chainId])) {
+    if (utils.getAddress(key) !== key || TOKEN_LIST[chainId][key].address !== key) {
+      if (TOKEN_LIST[chainId][key].wrappingMethod === 'Wrap Native' && !TOKEN_LIST[chainId]['0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'].address){
+        console.log(key, utils.getAddress(key));
+        throw new Error("Native Token is missing");
+      }
       console.log(key, utils.getAddress(key));
       throw new Error("wrong JSON format");
     }
