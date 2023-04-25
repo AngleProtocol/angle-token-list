7640b8aad030a2b53b8341ecf5a52e8ab3dddfe7
Pablo Veyrat <veyratpablo@gmail.com>
2023-04-25 19:57:54 +0200

    updating sdk

--- src/types.ts
+++ src/types.ts
@@ -51,4 +51,5 @@ export const joiSchema = Joi.object().keys({
   [ChainId.BSC]: Joi.object().pattern(/^/, tokenInfo),
   [ChainId.OPTIMISM]: Joi.object().pattern(/^/, tokenInfo),
   [ChainId.CELO]: Joi.object().pattern(/^/, tokenInfo),
+  [ChainId.GNOSIS]: Joi.object().pattern(/^/, tokenInfo),
 });
